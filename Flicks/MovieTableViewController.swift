6f77978ba74e95e14523500a38b5d78948fb548a
Daniel Tsui <dtsui@uber.com>
2016-02-07 14:23:08 -0800

    initialize search bar

--- Flicks/MovieTableViewController.swift
+++ Flicks/MovieTableViewController.swift
@@ -8,9 +8,31 @@
 
 import UIKit
 
-class MovieTableViewController: UITableViewController {
+class MovieTableViewController: UITableViewController, UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
+    var searchController : UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initSearchBar()
+
+    }
+    
+    private func initSearchBar() {
+        searchController = UISearchController(searchResultsController:  nil)
         
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        searchController.searchBar.delegate = self
+        
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = true
+        
+        navigationItem.titleView = searchController.searchBar
+        
+        definesPresentationContext = true
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        print("search was updated \(searchController.searchBar.text)")
     }
 }
