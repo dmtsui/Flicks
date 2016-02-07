d96ca22082a1221a805241e11358e114d9ce7786
Daniel Tsui <dtsui@uber.com>
2016-02-07 12:55:30 -0800

    add custom MovieTableViewController

---
+++ Flicks/MovieTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewController.swift
+//  Flicks
+//
+//  Created by Dan Tsui on 2/7/16.
+//  Copyright © 2016 Dan Tsui. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
