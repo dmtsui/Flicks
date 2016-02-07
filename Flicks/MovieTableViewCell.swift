e6b4c1759e2ad7f323e03de90fcc6c3867254533
Daniel Tsui <dtsui@uber.com>
2016-02-07 13:18:13 -0800

    add MovieTableViewCell

---
+++ Flicks/MovieTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieTableViewCell.swift
+//  Flicks
+//
+//  Created by Dan Tsui on 2/7/16.
+//  Copyright © 2016 Dan Tsui. All rights reserved.
+//
+
+import UIKit
+
+class MoviewTableViewCell: UITableViewCell {
+    
+}
